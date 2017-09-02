03df26841fc323655939bab0d6b238a31c0d43bb
Luke Morton <lukemorton.dev@gmail.com>
2017-09-02 14:48:08 +0100

    Reactivist branding

--- components/Header/components/Branding.js
+++ components/Header/components/Branding.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { style } from 'glamor'
+
+const brandingStyle = style({
+  fontWeight: 'strong',
+  letterSpacing: '.05em',
+  textTransform: 'uppercase'
+})
 
 export default () =>
-  <div>
+  <div {...brandingStyle}>
     Reactivist
   </div>
