a1a86370b3ea53a44004902901a0257126bcb23d
Luke Morton <lukemorton.dev@gmail.com>
2017-09-02 17:35:59 +0100

    Add some basic content to help

--- pages/help/index.js
+++ pages/help/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import app from '../../src/app'
-import { Page } from '../../components/Common'
+import { Content, Page } from '../../components/Common'
 
 export default app.page(() =>
   <Page title="Reactivist Help">
-    <h1>Getting Help</h1>
+    <Content>
+      <Content.ReadingColumn>
+        <h1>Getting Help</h1>
+      </Content.ReadingColumn>
+
+      <Content.ReadingAside>
+        <h4>Want to help?</h4>
+
+        <p>We would love for you to help out with knowledge and content.</p>
+
+        <p>Get in touch with Luke on <a href="https://twitter.com/LukeMorton">Twitter</a> or <a href="https://github.com/lukemorton/reactivist">GitHub</a></p>
+      </Content.ReadingAside>
+    </Content>
   </Page>
 )
