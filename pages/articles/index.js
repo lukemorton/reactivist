632d144b6b6feec87ee4fe0ccb6f2a77355f78a3
Luke Morton <lukemorton.dev@gmail.com>
2017-09-03 20:23:03 +0100

    Add show

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import { Link } from 'republic/react'
 import app from '../../src/app'
 import { Content, Page } from '../../components/Common'
 
 export default app.page(() =>
-  <Page title='Reactivist Articles'>
+  <Page title='Reactivist Article'>
     <Content>
-      <Content.ReadingPage title={<h1>An approach to component directory structure</h1>}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis ex neque, ut consequat lectus lobortis ornare. Curabitur finibus eget justo sit amet imperdiet. Curabitur malesuada nisl arcu, vel pellentesque tellus aliquam eu. Vestibulum id ex lacus. Vestibulum purus erat, dictum non risus vel, porta volutpat sapien. Integer diam tortor, rhoncus non hendrerit sit amet, dignissim in dui. Duis vitae imperdiet quam. Duis tempus, sem eu vulputate tincidunt, metus neque rutrum libero, quis volutpat risus lacus quis lectus. Aliquam ac ante pellentesque, malesuada ipsum vitae, tempor metus. Donec mollis justo ut mi laoreet, at laoreet dui pulvinar.</p>
+      <Content.ReadingColumn>
+        <h1>Latest Articles</h1>
 
-        <p>Praesent sit amet posuere turpis. Ut mi urna, molestie vel risus commodo, luctus porttitor felis. Vivamus placerat non nulla non luctus. Maecenas tortor ex, aliquet sit amet sagittis scelerisque, cursus et magna. Vivamus convallis consectetur tincidunt. Sed sed aliquam turpis, id gravida ligula. Sed imperdiet ante pulvinar urna aliquam, et ornare mauris convallis. Nam sed pulvinar arcu.</p>
+        <h2>
+          <Link action='articles#show' params={{ slug: 'example' }}>
+            Example article about cool stuff
+          </Link>
+        </h2>
 
-        <h2>Conclusion</h2>
+        <p>A short excerpt about the article is due to be here in some short while. It will eventually be very great.</p>
 
-        <p>Duis congue, sapien vehicula sagittis venenatis, odio magna vestibulum est, vel pharetra risus nulla a lacus. Quisque consequat cursus facilisis. Morbi imperdiet metus vel posuere luctus. Nulla iaculis vulputate mi, vitae ullamcorper leo viverra eget. Nunc ultrices egestas tempus. Phasellus lorem purus, pellentesque sed facilisis vitae, mattis id libero. Proin pharetra felis nec pharetra ullamcorper. Cras fringilla neque vitae nulla blandit ultrices. Suspendisse sed ullamcorper diam.</p>
-      </Content.ReadingPage>
+        <h2>
+          <Link action='articles#show' params={{ slug: 'another' }}>
+            Another example of one
+          </Link>
+        </h2>
+
+        <p>A short excerpt about the article is due to be here in some short while. It will eventually be very great.</p>
+      </Content.ReadingColumn>
+
+      <Content.ReadingAside>
+        <h4>Want to help?</h4>
+
+        <p>We would love for you to help out with knowledge and content.</p>
+
+        <p>Get in touch with Luke on <a href='https://twitter.com/LukeMorton'>Twitter</a> or <a href='https://github.com/lukemorton/reactivist'>GitHub</a></p>
+      </Content.ReadingAside>
     </Content>
   </Page>
 )
--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ import Republic, { route } from 'republic/next'
 export default new Republic(
   route.page('/', 'home#index'),
   route.page('/articles', 'articles#index'),
+  route.page('/articles/:slug', 'articles#show'),
   route.page('/help', 'help#index')
 )
