ca157ab4edea1b602238a5f5401a677a35c3f519
Luke Morton <lukemorton.dev@gmail.com>
2017-09-09 11:51:25 +0100

    Improve information on help section

--- pages/help/index.js
+++ pages/help/index.js
@@ -7,15 +7,25 @@ export default app.page(() =>
     <Content>
       <Content.ReadingColumn>
         <h1>Getting Help</h1>
-      </Content.ReadingColumn>
 
-      <Content.ReadingAside>
-        <h4>Want to help?</h4>
+        <h2>Have a question or issue with an article?</h2>
+
+        <p>
+          If there is something that doesn't quite make sense to you in an article, you're likely not the only one. Please take the time, if you can, to open an issue on our <a href='https://github.com/lukemorton/reactivist'>GitHub</a>. Make sure you link to the article :)
+        </p>
+
+        <h2>Want to say hello?</h2>
+
+        <p>
+          Feel free to say hi to Luke on <a href='https://twitter.com/LukeMorton'>Twitter</a> too.
+        </p>
+
+        <h2>Want to help?</h2>
 
         <p>We would love for you to help out with knowledge and content.</p>
 
         <p>Get in touch with Luke on <a href='https://twitter.com/LukeMorton'>Twitter</a> or <a href='https://github.com/lukemorton/reactivist'>GitHub</a></p>
-      </Content.ReadingAside>
+      </Content.ReadingColumn>
     </Content>
   </Page>
 )
