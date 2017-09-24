40214196a72a43e9d38b8e247c2b7e7212b6cded
Luke Morton <lukemorton.dev@gmail.com>
2017-09-24 18:18:11 +0100

    Lint

--- src/articles/LoadArticle.js
+++ src/articles/LoadArticle.js
@@ -5,7 +5,7 @@ const ARTICLES_ORIGIN = process.env.ARTICLES_ORIGIN
 async function LoadArticles () {
   let articleUrl = ARTICLES_ORIGIN + '/static/articles/index.json'
   const res = await fetch(articleUrl)
-  return await res.json()
+  return res.json()
 }
 
 export default async function LoadArticle ({ params }) {
