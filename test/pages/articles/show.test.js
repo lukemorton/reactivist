75fa6a03e834d9c9cbec6f6592d18becc267ba40
Luke Morton <lukemorton.dev@gmail.com>
2017-09-30 17:26:40 +0100

    Test article#show

---
+++ test/pages/articles/show.test.js
@@ -0,0 +1,25 @@
+import { mountPage } from 'republic/test-next'
+import nock from 'nock'
+import ArticlesShowPage from '../../../pages/articles/show'
+
+function mockArticleIndexRequest () {
+  nock(process.env.ARTICLES_ORIGIN)
+    .get('/static/articles/index.json')
+    .reply(200, {
+      'test-article': {
+        title: {
+          html: '<h1>Cool title</h1>',
+          plain: 'Cool title'
+        },
+        content: {}
+      }
+    })
+}
+
+describe('articles#index', () => {
+  it('should have title', async () => {
+    mockArticleIndexRequest()
+    const page = await mountPage(ArticlesShowPage, 'articles#show', { slug: 'test-article' })
+    expect(page.find('Page')).toHaveProp('title', 'Cool title – Reactivist')
+  })
+})
