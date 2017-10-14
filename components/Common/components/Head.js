3a31bb45fc5ba98234e0ca6547a9c4db53ab631f
Luke Morton <lukemorton.dev@gmail.com>
2017-10-14 17:04:06 +0100

    Fix attribute

--- components/Common/components/Head.js
+++ components/Common/components/Head.js
@@ -7,7 +7,7 @@ const defaultOGImage = ''
 
 export default (props) =>
   <NextHead>
-    <meta charset='utf-8' />
+    <meta charSet='utf-8' />
     <title>{props.title || ''}</title>
     <meta name='description' content={props.description || defaultDescription} />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
