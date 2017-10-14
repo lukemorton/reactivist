c03eb336c398e6776ae9aa9c710e494d3cf96273
Luke Morton <lukemorton.dev@gmail.com>
2017-10-14 14:55:23 +0100

    Log port

--- bin/start.js
+++ bin/start.js
@@ -3,4 +3,7 @@ import createServer from '../src/createServer'
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 
-createServer(dev).then((server) => server.listen(port))
+createServer(dev).then((server) => {
+  console.log(`Listening on port ${port} DEV=${dev}`)
+  server.listen(port)
+})
