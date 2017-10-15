5a2974894638d911cbe8cf04d3f2eed1bea4d220
Luke Morton <lukemorton.dev@gmail.com>
2017-10-15 13:12:14 +0100

    Use inbuilt republic cli

--- bin/start.js
+++
@@ -1,9 +0,0 @@
-import createServer from '../lib/createServer'
-
-const port = process.env.PORT || 3000
-const dev = process.env.NODE_ENV !== 'production'
-
-createServer(dev).then((server) => {
-  console.log(`Listening on port ${port} DEV=${dev}`)
-  server.listen(port)
-})
